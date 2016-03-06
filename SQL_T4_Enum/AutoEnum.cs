3dcf93c6735d43b8f02c77d67cbf4365a3ba4d6d
correiadefreitas <correiadefreitas@gmail.com>
2016-03-06 00:46:23 +0000

    Working example

--- SQL_T4_Enum/AutoEnum.cs
+++ SQL_T4_Enum/AutoEnum.cs
@@ -1,15 +1,16 @@
-//Do NOT CHANGE THIS FILE - Auto generated code from AppAutoEnums.tt template
+/// Author: José Pedro Correia de Freitas
+//Do NOT CHANGE THIS FILE - Auto generated code from AutoEnums.tt template
 //To generate this file, open .tt file and save it
 namespace model.Enums {
 
 	/// <summary>
-	/// Schema enum   : model.Enums
-	/// Nome enum     : ThisEnum
-	/// Tabela        : dbo.tbl
-	/// Coluna Valor  : id
-	/// Coluna Texto  : txt
+	/// Namespace    : model.Enums
+	/// Name         : ThisEnum
+	/// Type         : int
+	/// Table        : dbo.tbl
+	/// Value column : id
+	/// Text column  : txt
     /// </summary>
-
 	public enum ThisEnum 
 	{
 		FirstItem = 1,
